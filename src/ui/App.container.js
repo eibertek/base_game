502af53cc7340b9efa62d1509abdf4b130d1b37d
eibermanm <mariano.eiberman@gmail.com>
2017-11-19 20:55:51 -0300

    commiting all

---
+++ src/ui/App.container.js
@@ -0,0 +1,37 @@
+import App from "./App.jsx";
+import {connect} from 'react-redux';
+import gameConfig from './gameConfig';
+import components from './utils/libray';
+import { withRouter } from 'react-router-dom'
+
+const loginPageProps = (state, props) => {
+    return {
+        loginComponent: components['login'],
+        screens: [{mockData:state,}],
+    };
+}
+
+const normalProps = (state, props) => {
+    const componentsWillUse = gameConfig.screen.map( screen => components[screen.component]);
+    return {
+        characters: state.charStructure.characters,
+        screens: gameConfig.screen,
+        componentsWillUse,
+        screenNumber: props.screenNumber ? props.screenNumber : 0,
+    };
+}
+
+const mapStateToProps = (state, props) => {
+    const loggedIn = !!state.login;
+    if(loggedIn === true) {
+        return normalProps(state, props);
+    }else{
+        return loginPageProps(state, props);
+    }
+};
+
+export default withRouter(connect(mapStateToProps,
+        (dispatch)=>{return {
+            login: () => dispatch({ type:'LOGIN', login:true}),
+        }}
+    )(App));
