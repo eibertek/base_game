8499315b4d84d069e3d8d2154fcf742f5d723034
eibertek <mariano.eiberman@gmail.com>
2017-12-14 22:59:47 -0300

    change the way of passing actions to components

--- src/ui/commons/title.component/index.jsx
+++ src/ui/commons/title.component/index.jsx
@@ -4,8 +4,12 @@ import { PureComponent } from 'react';
 export default class titleComponent extends PureComponent {
 
     render() {
-        const { className, onClick, text} = this.props;
-        return <div onClick={onClick} className={className} >{text}</div>
+        const { className, onClick, text, actions, action } = this.props;
+        const onCustomClick = (evt) => {
+            actions[action](1);
+            onClick(evt);
+        }
+        return <div onClick={onCustomClick} className={className} >{text}</div>
     }
 }
 
