b5d5ea6285145aef3e2d746cd7a1b5880de31cea
eibertek <mariano.eiberman@gmail.com>
2017-12-14 22:54:30 -0300

    change the way of passing actions to components

--- src/ui/screen.component/index.jsx
+++ src/ui/screen.component/index.jsx
@@ -1,18 +1,19 @@
 import { Component, cloneElement } from 'react';
 export default class Render extends Component {
 
+    onCustomClick(clickObject) {
+        return (evt) => {
+            this.props.actions[clickObject.action](1);
+            return clickObject.event(evt);
+        }
+    }
+
   render(){
     const { childComponents, actions, ...props } = this.props;
-      const onCustomClick = (actions, clickObject) => {
-          return (evt) => {
-              actions[clickObject.action](1);
-              return clickObject.event(evt);
-          }
-      }
     return <div>
         {childComponents.map( c => {
           console.log(c.props);
-          return cloneElement(c, { actions, onClick: onCustomClick(actions, c.onClick) })
+          return cloneElement(c, { actions, onClick: this.onCustomClick(c) })
         })}
     </div>
   }
